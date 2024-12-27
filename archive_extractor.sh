378a1bea34b7a487d39d74fefa609ccd2fc2ab49
rysndavjd <rysndavjd@protonmail.com>
2024-12-27 21:19:16 +0200

    removed comments

--- archive_extractor.sh
+++ archive_extractor.sh
@@ -27,89 +27,24 @@ extract() {
 
 case "$TYPE" in 
     *Zip*)
-        #if [[ $(basename $FILE) == .* ]]; then
-        #    file=$(basename $FILE)
-        #    file=${file#.}
-        #    file=${file%.*}
-        #    mkdir -p "$OUT/.$file"
-        #    7zz -y x "$FILE" -o"$OUT/.$file"
-        #else
-        #    file=$(basename $FILE)
-        #    file=${file%.*}
-        #    mkdir -p "$OUT/$file"
-        #    7zz -y x "$FILE" -o"$OUT/$file"
-        #fi
         out_file=$(extract)
         7zz -y x "$FILE" -o"$out_file"
     ;;
     *gzip*) 
-        #if [[ $(basename $FILE) == .* ]]; then
-        #    file=$(basename $FILE)
-        #    file=${file#.}
-        #    file=${file%.*}
-        #    mkdir -p "$OUT/.$file"
-        #    7zz -y x "$FILE" -o"$OUT/.$file"
-        #else
-        #    file=$(basename $FILE)
-        #    file=${file%.*}
-        #    echo "$OUT/$file"
-        #    mkdir -p "$OUT/$file"
-        #    7zz -y x "$FILE" -o"$OUT/$file"
-        #fi
         out_file=$(extract)
         7zz -y x "$FILE" -o"$out_file"
     ;;
     *tar\ archive*) 
-        #if [[ $(basename $FILE) == .* ]]; then
-        #    file=$(basename $FILE)
-        #    file=${file#.}
-        #    file=${file%.*}
-        #    mkdir -p "$OUT/.$file"
-        #    cd "$OUT/$file"
-        #    tar -xf "$FILE" --overwrite
-        #    cd $CPWD
-        #else
-        #    file=$(basename $FILE)
-        #    file=${file%.*}
-        #    mkdir -p "$OUT/$file"
-        #    cd "$OUT/$file"
-        #    tar -xf "$FILE" --overwrite
-        #    cd $CPWD
-        #fi
         out_file=$(extract)
         cd "$out_file"
         tar -xf "$FILE" --overwrite
         cd $CPWD
     ;;
     *7-zip*)
-        #if [[ $(basename $FILE) == .* ]]; then
-        #    file=$(basename $FILE)
-        #    file=${file#.}
-        #    file=${file%.*}
-        #    mkdir -p "$OUT/.$file"
-        #    7zz -y x "$FILE" -o"$OUT/.$file"
-        #else
-        #    file=$(basename $FILE)
-        #    file=${file%.*}
-        #    mkdir -p "$OUT/$file"
-        #    7zz -y x "$FILE" -o"$OUT/$file"
-        #fi
         out_file=$(extract)
         7zz -y x "$FILE" -o"$out_file"
     ;;
     *RAR*)
-        #if [[ $(basename $FILE) == .* ]]; then
-        #    file=$(basename $FILE)
-        #    file=${file#.}
-        #    file=${file%.*}
-        #    mkdir -p "$OUT/.$file"
-        #    7zz -y x "$FILE" -o"$OUT/.$file"
-        #else
-        #    file=$(basename $FILE)
-        #    file=${file%.*}
-        #    mkdir -p "$OUT/$file"
-        #    7zz -y x "$FILE" -o"$OUT/$file"
-        #fi
         out_file=$(extract)
         7zz -y x "$FILE" -o"$out_file"
     ;;
